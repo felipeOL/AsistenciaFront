a88e56ebae2ac07c4b6c030af5755856e28aba89
Felipe <felipe.olave.r@hotmail.com>
2022-05-01 20:56:47 -0400

    feat(project): added structure project.
    - created teacher,student,administration api
    - created teacher,student,administration state
    - created teacher,student,administration facade
    - created teacher,student,administration dashboard component.
    - created authentication service.
    - created login component
    - created navbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardAdministrationComponent } from './components/administration/dashboard-administration/dashboard-administration.component';
+import { DashboardStudentComponent } from './student/dashboard-student/dashboard-student.component';
+import { DashboardTeacherComponent } from './teacher/dashboard-teacher/dashboard-teacher.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DashboardAdministrationComponent,
+    DashboardStudentComponent,
+    DashboardTeacherComponent
+  ],
   imports: [
     CommonModule
   ]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    NavbarComponent
+  ],
   imports: [
     CommonModule
   ]
