82be5c16a91ac413207cb0b5ff2fcb256802fc7b
Felipe <felipe.olave.r@hotmail.com>
2022-05-05 01:08:06 -0400

    feat(teacher): add components.
    - courses component added
    - schedules component added
    - assistance component added.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { DashboardAdministrationComponent } from './components/administration/dashboard-administration/dashboard-administration.component';
 import {DashboardStudentComponent} from "./components/student/dashboard-student/dashboard-student.component";
 import { DashboardTeacherComponent } from './components/teacher/dashboard-teacher/dashboard-teacher.component';
+import { CoursesComponent } from './components/teacher/courses/courses.component';
+import { SchedulesComponent } from './components/teacher/schedules/schedules.component';
+import { AssistanceComponent } from './components/teacher/assistance/assistance.component';
 
 
 
@@ -11,6 +14,9 @@ import { DashboardTeacherComponent } from './components/teacher/dashboard-teache
     DashboardAdministrationComponent,
     DashboardStudentComponent,
     DashboardTeacherComponent,
+    CoursesComponent,
+    SchedulesComponent,
+    AssistanceComponent,
   ],
   imports: [
     CommonModule
