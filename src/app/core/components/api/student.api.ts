9212c92af3355c95565d13e1d02469a4d753bf5f
Felipe <felipe.olave.r@hotmail.com>
2022-05-10 11:30:31 -0400

    feat(student api): added methods

--- src/app/core/components/api/student.api.ts
+++ src/app/core/components/api/student.api.ts
@@ -1,7 +1,21 @@
 import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {AuthenticationService} from "../../../services/authentication.service";
 
 @Injectable()
 
 export class studentApi{
+  constructor(
+    private httpClient:HttpClient,
+    private authservice: AuthenticationService,
+  ) {
+  }
 
+  getCourses(){
+
+  }
+
+  checkAssistence(){
+
+  }
 }
