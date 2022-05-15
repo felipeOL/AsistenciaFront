00b4b5d94ce6239295a3f94ec83f2b35303fcc9a
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-05-15 19:37:31 -0400

    feat(Cursos Teacher)
    The View Cursos of the user Teacher now show their Courses

--- src/app/core/components/api/teacher.api.ts
+++ src/app/core/components/api/teacher.api.ts
@@ -1,7 +1,45 @@
 import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {Observable, of} from "rxjs";
+import {CourseResponseModel} from "../../../Models/CourseResponse.model";
+import {AdminURL} from "../../../Util/adminURL.model";
+import {AuthenticationService} from "../../../services/authentication.service";
 
 @Injectable()
 
-export class teacherApi{
+export class teacherApi
+{
+  constructor
+  (
+    private httpclient: HttpClient,
+    private authservice: AuthenticationService
+  )
+  {}
 
+  getAllCourses(): Observable<CourseResponseModel[]>{
+    let courseList: CourseResponseModel[] = [];
+    let usuario = this.authservice.getUser();
+    this.httpclient.get<CourseResponseModel[]>(AdminURL.GET_ALL_COURSES, {
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer ' + usuario.token
+      }
+    }).subscribe(response => {
+        response.forEach(course => {
+          let nuevoCurso: CourseResponseModel = {
+            id: course.id,
+            codigo: course.codigo,
+            nombre: course.nombre,
+            seccion: course.seccion,
+            semestre: course.semestre,
+            bloque: course.bloque,
+            anio: course.anio,
+            profesor: course.profesor
+          }
+          courseList.push(nuevoCurso)
+        })
+      }
+    )
+    return of(courseList);
+  }
 }
