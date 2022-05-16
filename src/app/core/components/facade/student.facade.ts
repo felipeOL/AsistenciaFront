59467f64befd9d7616f27961a515f2e22184458b
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-05-15 21:58:48 -0400

    feat(ver cursos estudiante)
    was added View Cursos in Stundent interface

--- src/app/core/components/facade/student.facade.ts
+++ src/app/core/components/facade/student.facade.ts
@@ -1,7 +1,28 @@
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {CourseResponseModel} from "../../../Models/CourseResponse.model";
+import {studentState} from "../state/student.state";
+import {studentApi} from "../api/student.api";
 
 @Injectable()
 
-export class studentFacade{
+export class studentFacade
+{
+  constructor
+  (
+    private studentState: studentState,
+    private studentApi: studentApi
+  )
+  {}
 
+  get courses$():Observable<CourseResponseModel[]>{
+    return this.studentState.courses$();
+  }
+
+  updateCourse()
+  {
+    this.studentApi.getAllCourses().subscribe(curso => {
+      this.studentState.setCourses(curso);
+    })
+  }
 }
--- src/app/core/components/state/student.state.ts
+++ src/app/core/components/state/student.state.ts
@@ -1,7 +1,21 @@
 import {Injectable} from "@angular/core";
+import {BehaviorSubject, Observable} from "rxjs";
+import {CourseResponseModel} from "../../../Models/CourseResponse.model";
 
 @Injectable()
 
-export class studentState{
+export class studentState
+{
 
+  private coursesState$ = new BehaviorSubject<CourseResponseModel[]>([]);
+
+  courses$():Observable<CourseResponseModel[]>
+  {
+    return this.coursesState$.asObservable();
+  }
+
+  setCourses(courses:CourseResponseModel[])
+  {
+    this.coursesState$.next(courses);
+  }
 }
