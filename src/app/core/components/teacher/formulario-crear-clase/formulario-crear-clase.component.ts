82edcbfe98d87b0f2192da24e17d2451bd9a25bb
Felipe <felipe.olave.r@hotmail.com>
2022-05-18 20:13:06 -0400

    feat(crearClase): class create.

--- src/app/core/components/teacher/formulario-crear-clase/formulario-crear-clase.component.ts
+++ src/app/core/components/teacher/formulario-crear-clase/formulario-crear-clase.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialogRef} from "@angular/material/dialog";
+import {FormBuilder, Validators} from "@angular/forms";
+import {teacherFacade} from "../../facade/teacher.facade";
+import {CrearCourseModel} from "../../../../Models/CrearCourse.model";
+import {crearClaseModel} from "../../../../Models/crearClase.model";
 
 @Component({
   selector: 'app-formulario-crear-clase',
@@ -7,9 +12,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioCrearClaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogref: MatDialogRef<FormularioCrearClaseComponent>,
+    private formBuilder: FormBuilder,
+    private teacherFacade: teacherFacade,
+  ) { }
+
+  courseForm = this.formBuilder.group({
+    idCurso: ['',Validators.required],
+    sala: ['', Validators.required],
+    modalidad: ['',Validators.required],
+    bloque: ['',Validators.required],
+    fecha: ['',Validators.required],
+  })
 
   ngOnInit(): void {
   }
+
+  public closeDialog():void
+  {
+    this.dialogref.close()
+  }
+
+  addCourse():void
+  {
+    let newCourse : crearClaseModel = {
+      idCurso: this.courseForm.value.idCurso.toString(),
+      sala: this.courseForm.value.sala.toString(),
+      modalidad: this.courseForm.value.sala.toString(),
+      bloque: this.courseForm.value.bloque.toString(),
+      fecha: this.courseForm.value.fecha.toString(),
+  }
+
+
+    this.closeDialog()
+  }
+
 
 }
