2b42022d6c5e03944c815845e0c88e100c8e7169
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-05-30 20:12:28 -0400

    feat(httpinterceptor)
    needs to be tested but "it works".

--- src/app/services/http-interceptor.service.ts
+++ src/app/services/http-interceptor.service.ts
@@ -1,18 +1,38 @@
 import { Injectable } from '@angular/core';
-import {HttpHandler, HttpInterceptor, HttpRequest, HttpEvent} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpHandler, HttpInterceptor, HttpRequest, HttpEvent, HttpErrorResponse} from "@angular/common/http";
+import {catchError, Observable} from "rxjs";
+import {MatDialog} from "@angular/material/dialog";
+import {ErrorDialogComponent} from "../shared/components/dialogs/error-dialog/error-dialog.component";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpInterceptorService implements HttpInterceptor{
 
-  constructor() { }
+  constructor(
+    private dialog: MatDialog
+  ) { }
 
   // @ts-ignore
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>
   {
-    console.log('paso por aqui XD')
-    return next.handle(req);
+    // @ts-ignore
+    return next.handle(req).pipe(
+      catchError( err => this.capturaError(err))
+    );
+  }
+
+  // @ts-ignore
+  capturaError( error: HttpErrorResponse):string
+  {
+    console.log(error)
+    this.dialog.open(ErrorDialogComponent, {
+      data:
+        {
+          titulo: error.statusText,
+          contenido: error.error
+        }
+    })
+    return "e"
   }
 }
