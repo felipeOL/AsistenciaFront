660162dfd5b8a6c9e910da171f84ec52e834102e
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2022-06-07 16:27:13 -0400

    feat(validate the assistance button )
    different cases were added for the asistence button, out of hour, asistence was mark and mark asistence

--- src/app/core/components/student/student-assistance/student-assistance.component.ts
+++ src/app/core/components/student/student-assistance/student-assistance.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable} from "rxjs";
 import {classReponse} from "../../../../Models/classReponse.model";
 import {studentFacade} from "../../facade/student.facade";
+import {flatten} from "@angular/compiler";
 
 @Component({
   selector: 'app-student-assistance',
@@ -30,6 +31,15 @@ export class StudentAssistanceComponent implements OnInit {
     console.log(id)
     this.studentFacade.asistir(id,this.fechaActual);
     this.clases$ = this.studentFacade.clases$;
+  }
+
+  public validarHora(fecha: Date):boolean
+  {
+    let fechaClase = new Date(fecha)
+    let horaActual = new Date();
+    let horaMaxima = new Date(fechaClase.getFullYear(), fechaClase.getMonth(), fechaClase.getDate(), fechaClase.getHours()+1, fechaClase.getMinutes());
+    return horaActual >= fechaClase && horaActual <= horaMaxima;
+
   }
 
 }
