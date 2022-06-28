95250742b45f29016c3306c6834a036ce4328b5a
Felipe <felipe.olave.r@hotmail.com>
2022-06-28 12:56:50 -0400

    feat(sheet-courses): has been added html and methods

--- src/app/shared/components/sheet-courses/sheet-courses.component.ts
+++ src/app/shared/components/sheet-courses/sheet-courses.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from "xlsx";
+
+type AOA = any[][];
 
 @Component({
   selector: 'app-sheet-courses',
@@ -6,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sheet-courses.component.scss']
 })
 export class SheetCoursesComponent implements OnInit {
+  data: AOA = [];
+  wopts: XLSX.WritingOptions = {bookType: 'xlsx', type: 'array'};
+  fileName: string = 'SheetJS.xlsx';
+
+  onFileChange(evt: any) {}
+
+  export(): void {}
 
   constructor() { }
 
