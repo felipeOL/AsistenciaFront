213d06ce5c6039542b49212cfba819f60a37ceaf
Felipe <felipe.olave.r@hotmail.com>
2022-06-29 00:56:45 -0400

    feat(new-students-excel): has been created dialog

---
+++ src/app/core/components/teacher/result-creation-students/result-creation-students.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-creation-students',
+  templateUrl: './result-creation-students.component.html',
+  styleUrls: ['./result-creation-students.component.scss']
+})
+export class ResultCreationStudentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/services/new-students-excel.service.ts
+++ src/app/services/new-students-excel.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import {teacherFacade} from "../core/components/facade/teacher.facade";
+import {CreationStudentResponse} from "../Models/creationStudentResponse.model";
+import {MatDialog, MatDialogConfig} from "@angular/material/dialog";
+import {
+  ResultCreationStudentsComponent
+} from "../core/components/teacher/result-creation-students/result-creation-students.component";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewStudentsExcelService {
   correos:string[] = [];
-  constructor(private teacherFacade:teacherFacade) { }
+  constructor(
+    private teacherFacade:teacherFacade,
+    private dialog:MatDialog,
+              ) { }
+  responseRequest:CreationStudentResponse[] = [];
 
   addCorreo(correo:string){
     this.correos.push(correo);
@@ -18,7 +27,20 @@ export class NewStudentsExcelService {
 
   cargarExcel(){
     this.teacherFacade.createNewStudent(this.correos).subscribe(response => {
-      console.log(response);
+      this.responseRequest.push(response);
+    })
+    this.VisualizarRespuestas()
+  }
+
+  VisualizarRespuestas(){
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = "50%";
+    dialogConfig.data = this.responseRequest;
+    const dialogVal = this.dialog.open(ResultCreationStudentsComponent,dialogConfig);
+    dialogVal.afterClosed().subscribe( res => {
+      dialogVal.close();
     })
   }
 }
